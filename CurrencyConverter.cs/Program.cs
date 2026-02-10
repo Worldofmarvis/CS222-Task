c4e16d92525ae953463a2a501301f69f68cf1567
Worldofmarvis <vistoedmar25@gmail.com>
2026-02-10 02:57:40 -0500

    Add files via upload

---
+++ CurrencyConverter.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class CurrencyConverter{
+    public static void Main(string[] args){
+        Console.Write("Enter amount in USD: ");
+        double amountInUSD = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double exchangeRate = Convert.ToDouble(Console.ReadLine());
+        double amountInEUR = amountInUSD * exchangeRate;
+        Console.WriteLine($"Amount in EUR: {amountInEUR:F2}");
+
+    }
+
+
+}
---
+++ InterestCalculator.cs/InterestCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class InterestCalculator
+{
+       public static void Main()
+    {
+        Console.Write("Enter principal amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter rate of interest: ");
+        double annualRate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter time in years: ");
+        double time = Convert.ToDouble(Console.ReadLine());
+
+        double interest = (principal * annualRate * time) / 100;
+
+        Console.Write($"Simple Interest: {interest:F2}");
+
+
+
+
+    }
+    
+
+}
